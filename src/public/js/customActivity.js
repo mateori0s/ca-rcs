058ef3ac8b7a55b008a9a9dc4f9fdbefd1390a3b
mateori0s <mateorios@live.com>
2024-11-12 17:17:23 -0300

    delete caMode

--- src/public/js/customActivity.js
+++ src/public/js/customActivity.js
@@ -29,53 +29,14 @@ define(['postmonger'], (Postmonger) => {
             data.arguments.execute.inArguments.length > 0
         ) ? data.arguments.execute.inArguments : [];
 
-
-        const smsActionArg = inArguments.find(arg => arg.smsAction);
-        if (smsActionArg && smsActionArg.smsAction && ['send', 'save'].includes(smsActionArg.smsAction)) {
-            document.getElementById(`sms-action-${smsActionArg.smsAction}`).checked = true;
-            if (smsActionArg.smsAction === 'send') setSendAction();
-            else if (smsActionArg.smsAction === 'save') setSaveAction();
-        }
-
-        const caModeArg = inArguments.find(arg => arg.caMode);
-        if (caModeArg && caModeArg.caMode && ['independent', 'dependent', 'data-extension'].includes(caModeArg.caMode)) {
-            document.getElementById(`mode-${caModeArg.caMode}`).checked = true;
-            if (caModeArg.caMode === 'independent') setIndependentMode();
-            else if (caModeArg.caMode === 'dependent') setDependentMode();
-            else if (caModeArg.caMode === 'data-extension') setDataExtensionMode();
-        }
-
-        let caMode = getCaMode();
-        if (caMode === 'independent') {
-            const mensajeTraducidoArg = inArguments.find(arg => arg.mensajeTraducido);
-            if (mensajeTraducidoArg) {
-                document.getElementById('mensajeIndependiente').value = mensajeTraducidoArg.mensajeTraducido;
-            }
-        } else if (caMode === 'data-extension') {
-            const dataExtensionMessageColumnArg = inArguments.find(arg => arg.dataExtensionMessageColumn);
-            if (dataExtensionMessageColumnArg) {
-                document.getElementById('dataExtensionMessageColumn').value = dataExtensionMessageColumnArg.dataExtensionMessageColumn;
-            }
-        }
-
         const dataExtensionArg = inArguments.find(arg => arg.dataExtension);
         if (dataExtensionArg) document.getElementById('dataExtension').value = dataExtensionArg.dataExtension;
 
-        const expireDateModifierArg = inArguments.find(arg => arg.expireDateModifier);
-        if (expireDateModifierArg) document.getElementById("expire-date-modifier").value = expireDateModifierArg;
-
-        const saveActionCountryArg = inArguments.find(arg => arg.saveActionCountry);
-        if (saveActionCountryArg) {
-            document.getElementById(`save-action-country-${saveActionCountryArg.saveActionCountry}`).checked = true;
-            setOrigin(saveActionCountryArg.saveActionCountry);
-        }
     });
 
     connection.on('requestedInteraction', (payload) => {
-        let caMode = getCaMode();
-        if (caMode === 'dependent') {
-
-            
+        caMode = 'dependent'
+        if (caMode === 'dependent') {    
             let selectedValue;
             // determine the selected item (if there is one)
             if (activity.arguments.execute.inArguments) {
@@ -126,41 +87,21 @@ define(['postmonger'], (Postmonger) => {
     });
 
     connection.on('clickedNext', () => { // Save function within MC.
-        const caMode = getCaMode();
 
         const dataExtension = document.getElementById('dataExtension').value;
         let dataExtensionMessageColumn;
 
         let mensajeTraducido;
-        if (caMode === 'dependent') {
-            const select = document.getElementById("messageActivity");
-            mensajeTraducido = `{{Interaction.${select.options[select.selectedIndex].value}.mensajeTraducido}}`;
-        } else if (caMode === 'independent') {
-            mensajeTraducido = document.getElementById("mensajeIndependiente").value;
-        } else if (caMode === 'data-extension') {
-            dataExtensionMessageColumn = document.getElementById("dataExtensionMessageColumn").value;
-            mensajeTraducido = `{{Contact.Attribute."${dataExtension}".${dataExtensionMessageColumn}}}`;
-        }
 
         const cellularNumber = `{{Contact.Attribute."${dataExtension}".cellular_number}}`;
-        const smsAction = getSmsAction();
-
+    
         let expireDateModifier = document.getElementById("expire-date-modifier").value ;
-
         let saveActionCountry = null;
-        for (const country of ['AR', 'UY', 'PY']) {
-            if (document.getElementById(`save-action-country-${country}`).checked) {
-                saveActionCountry = country;
-                break;
-            }
-        }
         
         activity['arguments'].execute.inArguments = [
             { dataExtension: dataExtension ? dataExtension : null },
             { mensajeTraducido: mensajeTraducido ? mensajeTraducido : null },
             { cellularNumber: cellularNumber ? cellularNumber : null },
-            { smsAction: smsAction ? smsAction : null },
-            { caMode: caMode ? caMode : null },
             { dataExtensionMessageColumn: dataExtensionMessageColumn ? dataExtensionMessageColumn : null },
             { expireDateModifier: expireDateModifier ? expireDateModifier : null },
             { saveActionCountry: saveActionCountry ? saveActionCountry : null }
@@ -180,14 +121,6 @@ define(['postmonger'], (Postmonger) => {
     });
 });
 
-function getSmsAction() {
-    let smsAction;
-    for (const action of ['send', 'save']) {
-        if (document.getElementById(`sms-action-${action}`).checked) smsAction = action;
-    }
-    return smsAction;
-}
-
 function setSendAction() {
     document.getElementById('expireDateModifierDiv').style.display = 'none';
     connection.trigger('requestInteraction');
@@ -198,31 +131,3 @@ function setSaveAction() {
     connection.trigger('requestInteraction');
 }
 
-function setIndependentMode() {
-    document.getElementById("dependentModeOptionsDiv").style.display = "none";
-    document.getElementById("independentModeOptionsDiv").style.display = "flex";
-    document.getElementById("dataExtensionModeOptionsDiv").style.display = "none";
-    connection.trigger("requestInteraction");
-}
-
-function setDependentMode() {
-    document.getElementById("dependentModeOptionsDiv").style.display = "flex";
-    document.getElementById("independentModeOptionsDiv").style.display = "none";
-    document.getElementById("dataExtensionModeOptionsDiv").style.display = "none";
-    connection.trigger("requestInteraction");
-}
-
-function setDataExtensionMode() {
-    document.getElementById("dependentModeOptionsDiv").style.display = "none";
-    document.getElementById("independentModeOptionsDiv").style.display = "none";
-    document.getElementById("dataExtensionModeOptionsDiv").style.display = "flex";
-    connection.trigger("requestInteraction");
-}
-
-function getCaMode() {
-    let caMode;
-    for (const mode of ['independent', 'dependent', 'data-extension']) {
-        if (document.getElementById(`mode-${mode}`).checked) caMode = mode;
-    }
-    return caMode;
-}
