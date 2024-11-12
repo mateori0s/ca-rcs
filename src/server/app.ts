edecebc7427b4a1f82ce69a0cad527aca49f5b3d
mateori0s <mateorios@live.com>
2024-11-12 16:36:05 -0300

    puerto 3000

--- src/server/app.ts
+++ src/server/app.ts
@@ -12,6 +12,11 @@ import * as http from 'http';
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 // Configure Express 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
